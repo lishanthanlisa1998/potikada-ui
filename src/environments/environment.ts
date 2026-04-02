3ba14811a60edf6ba654da0b92c8a48d57430530
Lishanthan Vijayakumar <lishanthan@yoors.local>
2026-04-02 20:52:46 +0530

    chage phone number

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.potikada.lk/api',
+  apiUrl: 'https://potikada-api-production.up.railway.app/api',
   cloudinary: {
     cloudName: 'dd57xykdy',
     uploadPreset: 'potikada',
