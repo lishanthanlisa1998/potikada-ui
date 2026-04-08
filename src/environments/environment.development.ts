bdd1a8592c3675be055e734f000e5539fb0a5327
Lishanthan Vijayakumar <lishanthan@yoors.local>
2026-04-08 15:48:04 +0530

    remove add to cart

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://api.potikada.lk/api',
+  apiUrl: 'http://127.0.0.1:8000/api',
   cloudinary: {
     cloudName:    'dd57xykdy',
     uploadPreset: 'potikada',
